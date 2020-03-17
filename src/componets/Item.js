18b2ccc12d88641d7ffb954dd5f2d0fc880c193d
Pawel Borowik <pawelbor1@op.pl>
2020-03-17 19:42:24 +0100

    arrangement code in item.scss

--- src/componets/Item.js
+++ src/componets/Item.js
@@ -2,33 +2,25 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCartArrowDown, faTrashAlt } from '@fortawesome/free-solid-svg-icons'
 
-
-
 function Item(props) {
-
-    // const basket = <FontAwesomeIcon icon={faCoffee} />
-
 
     return (
 
         <li className="listli">
-            <div className={`list_basket ${props.item.checkbox ? "important" : ""} ${props.item.bought ? "bought" : ""}`} onClick={() => props.handleBoughtItem(props.item.id)}>
-                <FontAwesomeIcon icon={faCartArrowDown} /></div>
-
+            <div className={`list_basket ${props.item.checkbox ? "important" : ""} ${props.item.bought ? "bought" : ""}`}
+                onClick={() => props.handleBoughtItem(props.item.id)}>
+                <FontAwesomeIcon icon={faCartArrowDown} />
+            </div>
             <div
                 className={`list_item ${props.item.checkbox ? "important" : ""} ${props.item.bought ? "bought" : ""}`}
                 onClick={() => props.handleBoughtItem(props.item.id)}>
                 {props.item.text}
             </div>
-
             <div
                 className="list_delete-icon"
                 onClick={() => props.handleRemovetItemFromList(props.item.id)}>
                 <FontAwesomeIcon icon={faTrashAlt} />
             </div>
-
-
-
         </li>
     );
 }
