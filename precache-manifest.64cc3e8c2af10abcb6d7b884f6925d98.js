47861ed81705b660612fa71f5bfddd2978f9dc8b
Pawel Borowik <pawelbor1@op.pl>
2020-03-17 21:35:47 +0100

    Updates

---
+++ precache-manifest.64cc3e8c2af10abcb6d7b884f6925d98.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa4ecd460b44d329c80d06f55a916506",
+    "url": "/shopping-list/index.html"
+  },
+  {
+    "revision": "967aa4bcbd983ddd6c6e",
+    "url": "/shopping-list/static/css/main.f4ef4f3e.chunk.css"
+  },
+  {
+    "revision": "7cf28cafc096b17f8081",
+    "url": "/shopping-list/static/js/2.bd1994ec.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/shopping-list/static/js/2.bd1994ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "967aa4bcbd983ddd6c6e",
+    "url": "/shopping-list/static/js/main.a25c82ca.chunk.js"
+  },
+  {
+    "revision": "90ea1ac884122a8a6a61",
+    "url": "/shopping-list/static/js/runtime-main.d599a343.js"
+  }
+]);
